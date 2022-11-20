5a452236e80fe04a662dd02d143d90bbe933b22c
Nelson Nwajie <nwajienelson@gmail.com>
2022-11-19 19:35:22 -0700

    new

--- backend-services.tf
+++ backend-services.tf
@@ -35,7 +35,7 @@ resource "aws_elasticache_cluster" "netrox-cache" {
   engine               = "memcached"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.memcached1.5"
+  parameter_group_name = "default.memcached1.6"
   port                 = 11211
   subnet_group_name    = aws_elasticache_subnet_group.netrox-ecache-subgrp.name
   security_group_ids   = [aws_security_group.netrox-backend-sg.id]
