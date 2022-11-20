63fdf4395f75f8ebfa2158e647a573ae1789e6fa
Nelson Nwajie <nwajienelson@gmail.com>
2022-11-19 19:05:44 -0700

    ammended some lines

--- beanstalk-app.tf
+++ beanstalk-app.tf
@@ -2,9 +2,4 @@ resource "aws_elastic_beanstalk_application" "netrox-prod" {
   name        = "netrox-prod"
   description = "beanstallk application for netrox-prod instance"
 
-  appversion_lifecycle {
-    service_role          = aws_iam_role.beanstalk_service.arn
-    max_count             = 128
-    delete_source_from_s3 = true
-  }
 }
